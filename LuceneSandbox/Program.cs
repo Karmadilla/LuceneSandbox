5c0fa2833ab9f4171c6a24067f7be7331b52b22f
DenysOlleik <dolleik@gmail.com>
2020-08-26 17:30:42 -0400

    Adding user input from console.

--- LuceneSandbox/Program.cs
+++ LuceneSandbox/Program.cs
@@ -22,36 +22,77 @@ namespace LuceneSandbox
             var dir = new RAMDirectory();
 
             //create an analyzer to process the text
-            var analyzer = new SimpleAnalyzer(luceneVersion);
+            var analyzer = new WhitespaceAnalyzer(luceneVersion);
 
-            ////create an index writer
+            //create an index writer
             var indexConfig = new IndexWriterConfig(luceneVersion, analyzer);
             var writer = new IndexWriter(dir, indexConfig);
 
-            writer.AddDocument(new Document { new TextField("Query", new { Query = "Best pants in chicago" }.Query, Field.Store.YES) });
-            writer.AddDocument(new Document { new TextField("Query", new { Query = "Best pants in detroit" }.Query, Field.Store.YES) });
-            writer.AddDocument(new Document { new TextField("Query", new { Query = "Worst pants in chicago" }.Query, Field.Store.YES) });
-            writer.AddDocument(new Document { new TextField("Query", new { Query = "Worst pants in detroit" }.Query, Field.Store.YES) });
-            writer.AddDocument(new Document { new TextField("Query", new { Query = "hello world pants" }.Query, Field.Store.YES) });
+            //writer.AddDocument(new Document { new TextField("Query", new { Query = "Best pants in chicago" }.Query, Field.Store.YES) });
+            //writer.AddDocument(new Document { new TextField("Query", new { Query = "Best pants in detroit" }.Query, Field.Store.YES) });
+            //writer.AddDocument(new Document { new TextField("Query", new { Query = "Worst pants in chicago" }.Query, Field.Store.YES) });
+            //writer.AddDocument(new Document { new TextField("Query", new { Query = "Worst pants in detroit" }.Query, Field.Store.YES) });
+            //writer.AddDocument(new Document { new TextField("Query", new { Query = "hello world pants" }.Query, Field.Store.YES) });
 
-            writer.Flush(triggerMerge: false, applyAllDeletes: false);
+            //writer.Flush(triggerMerge: false, applyAllDeletes: false);
 
-            var simpleQueryParser = new Lucene.Net.QueryParsers.Simple.SimpleQueryParser(analyzer, "Query");
-            simpleQueryParser.DefaultOperator = Occur.SHOULD;            
-            var query = simpleQueryParser.Parse("Best pants chicago -in");            
-
-            // re-use the writer to get real-time updates
-            var searcher = new IndexSearcher(writer.GetReader(applyAllDeletes: true));
-            var hits = searcher.Search(query, 20 /* top 20 */).ScoreDocs;
-
-            foreach (var hit in hits)
+            while (true)
             {
-                var foundDoc = searcher.Doc(hit.Doc);
-                foreach (var item2 in foundDoc.Fields)
+                Console.Clear();
+                Console.WriteLine("1. Index");
+                Console.WriteLine("2. Search");
+
+                string option = Console.ReadLine().ToString();
+
+                if (option == "1")
                 {
-                    Console.WriteLine("value: " + item2.GetStringValue() + " score: " + hit.Score);
+                    Console.Clear();
+                    Console.Write("Text to index: ");
+                    string textToIndex = Console.ReadLine();
+                    writer.AddDocument(new Document() { new TextField("Query", new { Query = textToIndex }.Query, Field.Store.YES) });
+                    writer.Flush(true, false);
+                }
+                else if (option == "2")
+                {
+                    Console.Clear();
+                    Console.Write("Text to search: ");
+                    string textToSearch = Console.ReadLine();
+                    
+                    var simpleQueryParser = new Lucene.Net.QueryParsers.Simple.SimpleQueryParser(analyzer, "Query");
+                    simpleQueryParser.DefaultOperator = Occur.SHOULD;
+                    var query = simpleQueryParser.Parse(textToSearch);
+                    var searcher = new IndexSearcher(writer.GetReader(false));
+                    var topDocs = searcher.Search(query, 10).ScoreDocs;
+
+                    foreach (var hit in topDocs)
+                    {
+                        var foundDoc = searcher.Doc(hit.Doc);
+                        foreach (var item2 in foundDoc.Fields)
+                        {
+                            Console.WriteLine("value: " + item2.GetStringValue() + " score: " + hit.Score);
+                        }
+                    }
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
                 }
             }
+
+            //var simpleQueryParser = new Lucene.Net.QueryParsers.Simple.SimpleQueryParser(analyzer, "Query");
+            //simpleQueryParser.DefaultOperator = Occur.SHOULD;
+            //var query = simpleQueryParser.Parse("Best pants chicago -in");            
+
+            //// re-use the writer to get real-time updates
+            //var searcher = new IndexSearcher(writer.GetReader(applyAllDeletes: true));
+            //var hits = searcher.Search(query, top 20).ScoreDocs;
+
+            //foreach (var hit in hits)
+            //{
+            //    var foundDoc = searcher.Doc(hit.Doc);
+            //    foreach (var item2 in foundDoc.Fields)
+            //    {
+            //        Console.WriteLine("value: " + item2.GetStringValue() + " score: " + hit.Score);
+            //    }
+            //}
         }
     }
 }
